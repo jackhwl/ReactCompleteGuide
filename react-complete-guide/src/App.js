374954de4cab3b6ffe580d7128c3a61d4ca580d3
Wenlin Huang <jackhwl@hotmail.com>
2019-09-16 08:08:16 -0400

    48 Working with Inline Styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -23,17 +23,36 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 29},
+        { name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
         {/* this.state.persons.forEach(person => {
         <Person name={this.person.name} age={this.person.age} />
           
         }); */}
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person click={this.switchNameHandler.bind(this, 'Max!')} name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person click={this.switchNameHandler.bind(this, 'Max!')} 
+                changed={this.nameChangedHandler}
+          name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
