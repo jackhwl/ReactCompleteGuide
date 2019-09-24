2806f8574750fcff025578dbe839c96155d98fd2
Wenlin Huang <jackhwl@hotmail.com>
2019-09-23 22:00:58 -0400

    90. Component Update Lifecycle (ffor props Changes)

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-    console.log('[Person.js] rendering...');
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...');
         // const rnd = Math.random();
 
-    // if (rnd > 0.7) {
-    //     throw new Error( 'Something went wrong' );
-    // }
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click} onChange={props.changed}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+        // if (rnd > 0.7) {
+        //     throw new Error( 'Something went wrong' );
+        // }
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click} onChange={this.props.changed}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
 }
 
-export default person;
+export default Person;
