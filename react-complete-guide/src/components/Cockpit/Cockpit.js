51b6f762b031bdf5b2c4b2a3debcb38531d01164
Wenlin Huang <jackhwl@hotmail.com>
2019-09-26 08:35:27 -0400

    110. Refs with React Hooks

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+
     useEffect(() => {
       console.log('[Cockpit.js] useEffect');
       // Http request...
       //const timer = 
-      setTimeout(()=> {alert('Saved data to cloud!')}, 1000);
+      //setTimeout(()=> {alert('Saved data to cloud!')}, 1000);
+      toggleBtnRef.current.click();
       return () => {
         //clearTimeout(timer);
         console.log('[Cockpit.js] cleanup work in useEffect');
@@ -35,7 +38,7 @@ const cockpit = (props) => {
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
+            <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>Toggle Persons</button>
         </div>
     );
 };
