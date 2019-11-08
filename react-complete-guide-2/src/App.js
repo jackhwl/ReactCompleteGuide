dbeb1d0ef617169ae798f82bdf351acb39c26f00
Wenlin Huang <jackhwl@hotmail.com>
2019-11-08 08:36:24 -0500

    30. Understanding JSX

--- react-complete-guide-2/src/App.js
+++ react-complete-guide-2/src/App.js
@@ -3,24 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hi, I'm react app</h1>
+  //   </div>
+  // );
+  return React.createElement('div', {className: 'App'}, 
+            React.createElement('h1', null, 'Hi, i\'m react app'))
 }
 
 export default App;
