a845b33d73db625cd1727f4bab84e6f758308f58
Wenlin Huang <jackhwl@hotmail.com>
2019-11-08 21:02:07 -0500

    35. Outputting Dynamic Content

--- react-complete-guide-2/src/App.js
+++ react-complete-guide-2/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi, I'm react app</h1>
-  //   </div>
-  // );
-  return React.createElement('div', {className: 'App'}, 
-            React.createElement('h1', null, 'Hi, i\'m react app'))
+  return (
+    <div className="App">
+      <h1>Hi, I'm react app</h1>
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'}, 
+  //           React.createElement('h1', null, 'Hi, i\'m react app'))
 }
 
 export default App;
---
+++ react-complete-guide-2/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Person = () => {
+    return (
+        <div>
+            I'm a Person! I'm {Math.floor(Math.random()*30)} years old.
+        </div>
+    )
+}
+
+export default Person
