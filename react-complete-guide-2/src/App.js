125c8e4d0da0590726c27432813fa992cf766b49
Wenlin Huang <jackhwl@hotmail.com>
2019-11-08 22:07:23 -0500

    42. Manipulating the State

--- react-complete-guide-2/src/App.js
+++ react-complete-guide-2/src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm react app</h1>
-      <Person name="Max" age="28" />
-      <Person name="Jack" age="42">My Hobbies: Racing</Person>
-      <Person name="Bill" age="22" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 27},
+      {name: 'Jim', age: 22},
+      {name: 'Jone', age: 33}
+    ]
+  }
+  switchNameHandler = () => {
+    this.setState({persons:[
+      {name: 'Maximilian', age: 29},
+      {name: 'Jim2', age: 22},
+      {name: 'Jone', age: 33}
+    ]})
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        { 
+          this.state.persons.map(p => 
+            <Person name={p.name} age={p.age} />
+          )
+        }
+
+        <Person name="Jack" age="42">My Hobbies: Racing</Person>
+        <Person name="Bill" age="22" />
+      </div>
+    );
+  }
   // return React.createElement('div', {className: 'App'}, 
   //           React.createElement('h1', null, 'Hi, i\'m react app'))
 }
