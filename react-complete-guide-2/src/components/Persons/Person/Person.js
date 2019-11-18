c00ddebfffea6c9a3e8c3274271a49f129c0219d
Wenlin Huang <jackhwl@hotmail.com>
2019-11-18 07:48:20 -0500

    106. Another Form of HOCs

--- react-complete-guide-2/src/components/Persons/Person/Person.js
+++ react-complete-guide-2/src/components/Persons/Person/Person.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import classes from './Person.module.css'
 import Aux from '../../../hoc/Auxiliary'
-import WithClass from '../../../hoc/WithClass'
+import withClass2 from '../../../hoc/withClass2'
 
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <WithClass classes={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a Person! I'm {this.props.name}, {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </WithClass>
+            </Aux>
         )
     }
 }
-export default Person
+export default withClass2(Person, classes.Person)
