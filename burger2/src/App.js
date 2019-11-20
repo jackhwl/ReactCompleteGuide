075e3f5dd19b4045f3fda75f29d5d5a3b882c098
Wenlin Huang <jackhwl@hotmail.com>
2019-11-20 09:15:22 -0500

    154. Creating a Layout Component

--- burger2/src/App.js
+++ burger2/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
