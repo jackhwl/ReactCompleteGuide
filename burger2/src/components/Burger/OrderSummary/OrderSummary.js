c0f4967102940dd33bfe72133821e94aaf08370b
Wenlin Huang <jackhwl@hotmail.com>
2019-11-24 14:17:46 -0500

    186. Changing the Folder Structure

--- burger2/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger2/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import Aux from '../../../hoc/Auxlib'
+import Aux from '../../../hoc/Auxlib/Auxlib'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component {
