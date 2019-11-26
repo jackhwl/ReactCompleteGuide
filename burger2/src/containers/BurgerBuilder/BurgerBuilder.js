5b908638c1111cdc46e97e615ef787290a420c3a
Wenlin Huang <jackhwl@hotmail.com>
2019-11-25 21:52:57 -0500

    211. Sending a POST Request

--- burger2/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger2/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -63,7 +64,23 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }    
     purchaeContinueHandler = () => {
-        alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Jack Huang',
+                address: {
+                    street: '3050 Del Mar Ave',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }    
     render() {
         const disabledInfo = {
