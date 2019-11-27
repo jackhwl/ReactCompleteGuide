8127ff991e39f356c56f74657ab26f311ff49b5b
Wenlin Huang <jackhwl@hotmail.com>
2019-11-27 09:04:40 -0500

    222. Preparing the Project For Routing

--- http-02/src/containers/Blog/Blog.js
+++ http-02/src/containers/Blog/Blog.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
 //import axios from 'axios'
-import axios from '../../axios'
+import Posts from './Posts/Posts'
 
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-    componentDidMount(){
-        console.log('blog')
-        axios.get('/posts')
-            .then((response => {
-                const posts = response.data.slice(0,4)
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Jack'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-                // console.log(response)
-            }))
-            .catch(error => this.setState({error: true}))
-    }
-
-    postSelectedHandler = (id) => {
-        console.log('blog id changed', id)
-        this.setState({selectedPostId: id})
-    }
-
     render () {
-        const posts = this.state.posts.map(post=> 
-            <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        )
+
         return (
             <div className="Blog">
                 <header>
@@ -51,9 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ http-02/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react'
+import axios from '../../../axios'
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+
+class Posts extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount(){
+        console.log('blog')
+        axios.get('/posts')
+            .then((response => {
+                const posts = response.data.slice(0,4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Jack'
+                    }
+                })
+                this.setState({posts: updatedPosts})
+                // console.log(response)
+            }))
+            .catch(error => this.setState({error: true}))
+    }
+
+    postSelectedHandler = (id) => {
+        console.log('blog id changed', id)
+        this.setState({selectedPostId: id})
+    }
+        
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if (!this.state.error){
+            posts = this.state.posts.map(post=> 
+                <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            )    
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )   
+    }
+
+}
+
+export default Posts
+
