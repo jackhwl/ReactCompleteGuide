572daa9eb4336fd0f3cf02b04b0765eb8827ca19
Wenlin Huang <jackhwl@hotmail.com>
2019-11-30 09:13:56 -0500

    244. Handling the 404 Case (Unknown Routes)

--- http-02/src/containers/Blog/Blog.js
+++ http-02/src/containers/Blog/Blog.js
@@ -42,7 +42,8 @@ class Blog extends Component {
                 <Switch>
                     { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render = {() => <h1>404 Not found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                     {/* <Route path="/:id" exact component={FullPost} /> */}
                 </Switch>
             </div>
