01ac19bf1a0f02b3749dae360c803e5631489510
Wenlin Huang <jackhwl@hotmail.com>
2019-12-01 21:36:24 -0500

    255. Passing Ingredients via Query Params

--- burger2/src/components/Burger/Burger.js
+++ burger2/src/components/Burger/Burger.js
@@ -3,14 +3,21 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    console.log('props.ingredients=', props.ingredients)
+    let ab = Object.keys(props.ingredients)
+        .map(igKey => [...Array(props.ingredients[igKey])].map((_,i) => i))
+    console.log('ab=',ab)
     let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => [...Array(props.ingredients[igKey])].map((_, i) =>
-            <BurgerIngredient key={igKey + i} type={igKey} />
-        ))
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => 
+                <BurgerIngredient key={igKey + i} type={igKey} />   
+            )
+        })
         .reduce((arr, el) => arr.concat(el),[])
-        if(transformedIngredients.length === 0){
-            transformedIngredients = <p>Please start adding ingredients!</p>
-        }
+    console.log(transformedIngredients)
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- burger2/src/containers/Checkout/Checkout.js
+++ burger2/src/containers/Checkout/Checkout.js
@@ -4,18 +4,32 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 class Checkout extends Component {
     state = {
         ingredients: {
-            salad: 1,
+            salad: 0,
             meat: 1,
             cheese: 1,
             bacon: 1
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        for (let param of query.entries()) {
+            //['salad', '1']
+            ingredients[param[0]] = param[1]
+        }
+        console.log(ingredients) 
+        this.setState({ingredients: ingredients})
+    }
+
     checkoutCancelHandler = () => {
         this.props.history.goBack()
     }    
+
     checkoutContinueHandler = () => {
         this.props.history.replace('/checkout/contact-data')
     }    
+
     render() {
         return (
             <div>
