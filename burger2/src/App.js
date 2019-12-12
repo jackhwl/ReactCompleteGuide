c73a7a6cb030a02703ec004f6a02917a3345cd34
Wenlin Huang <jackhwl@hotmail.com>
2019-12-12 09:16:12 -0500

    354. Adding an Auth Form

--- burger2/src/App.js
+++ burger2/src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 //import BurgerIngredient from './components/Burger/BurgerIngredient/BurgerIngredient';
 
 class App extends Component {
@@ -13,7 +14,8 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />            
-            <Route path="/orders" component={Orders} />            
+            <Route path="/orders" component={Orders} />    
+            <Route path="/auth" component={Auth} />         
             <Route path="/" component={BurgerBuilder} />          
           </Switch>
         </Layout>
