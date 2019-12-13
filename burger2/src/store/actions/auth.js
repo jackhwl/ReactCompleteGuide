697b42050e3e3c2810eb4c6f6e17b3407ddf1ba7
Wenlin Huang <jackhwl@hotmail.com>
2019-12-13 07:58:53 -0500

    357. Adding Sign-In

--- burger2/src/store/actions/auth.js
+++ burger2/src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart())
         const authData = {
@@ -29,7 +29,12 @@ export const auth = (email, password) => {
             password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC7TlFEBVh4GvRfelvTgjisnCW1HAXrY-M', authData)
+        let api_key = 'AIzaSyC7TlFEBVh4GvRfelvTgjisnCW1HAXrY=M';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + api_key;
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + api_key;
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response)
                 dispatch(authSuccess(response.data))
