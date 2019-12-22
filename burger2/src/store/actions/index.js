7fea8e0087fdb6d7f561ca15ab324318559a26f2
Wenlin Huang <jackhwl@hotmail.com>
2019-12-22 14:24:58 -0500

    449. Moving the BBuilder Side Effects into a Saga

--- burger2/src/store/actions/index.js
+++ burger2/src/store/actions/index.js
@@ -1,4 +1,5 @@
-export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder'
+export {addIngredient, removeIngredient, initIngredients, 
+    setIngredients, fetchIngredientsFailed} from './burgerBuilder'
 export { purchaseBurger, purchaseInit, fetchOrders } from './orders'
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, 
     authStart, authSuccess, authFail, checkAuthTimeout } from './auth'
--- burger2/src/store/sagas/index.js
+++ burger2/src/store/sagas/index.js
@@ -1,5 +1,6 @@
 import { takeEvery } from 'redux-saga/effects'
 import { logoutSaga, checkAuthTimeoutSage, authUserSaga, authCheckStateSaga } from './auth'
+import { initIngredientsSaga } from './burgerBuilder'
 import * as actionTypes from '../actions/actionTypes'
 
 export function* watchAuth() {
@@ -7,4 +8,8 @@ export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
 }
