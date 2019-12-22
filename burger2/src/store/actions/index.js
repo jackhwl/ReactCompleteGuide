1c7a0cfc978b8656cfd4ab9779bfea34a6540617
Wenlin Huang <jackhwl@hotmail.com>
2019-12-22 18:32:24 -0500

    450. Moving the Orders Side Effects into Sagas

--- burger2/src/store/actions/index.js
+++ burger2/src/store/actions/index.js
@@ -1,5 +1,7 @@
 export {addIngredient, removeIngredient, initIngredients, 
     setIngredients, fetchIngredientsFailed} from './burgerBuilder'
-export { purchaseBurger, purchaseInit, fetchOrders } from './orders'
+export { purchaseBurger, purchaseInit, fetchOrders, 
+    purchaseBurgerStart, purchaseBurgerSuccess, purchaseBurgerFail,
+    fetchOrdersSuccess, fetchOrdersStart, fetchOrdersFail } from './orders'
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, 
     authStart, authSuccess, authFail, checkAuthTimeout } from './auth'
