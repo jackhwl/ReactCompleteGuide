1323f54c5634a63a9b7a30dd8b223032df9c7b48
Wenlin Huang <jackhwl@hotmail.com>
2019-12-21 21:48:36 -0500

    446. Moving More Logic Into Sagas

--- burger2/src/store/sagas/index.js
+++ burger2/src/store/sagas/index.js
@@ -1,7 +1,8 @@
 import { takeEvery } from 'redux-saga/effects'
-import { logoutSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSage } from './auth'
 import * as actionTypes from '../actions/actionTypes'
 
 export function* watchAuth() {
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSage)
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
 }
