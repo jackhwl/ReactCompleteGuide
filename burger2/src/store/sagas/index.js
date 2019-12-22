1cc432bad2719b73dc7e49ee1920775dc66ba4ad
Wenlin Huang <jackhwl@hotmail.com>
2019-12-22 08:28:48 -0500

    448. Handling Auto-Sign-In with a Saga

--- burger2/src/store/sagas/index.js
+++ burger2/src/store/sagas/index.js
@@ -1,9 +1,10 @@
 import { takeEvery } from 'redux-saga/effects'
-import { logoutSaga, checkAuthTimeoutSage, authUserSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSage, authUserSaga, authCheckStateSaga } from './auth'
 import * as actionTypes from '../actions/actionTypes'
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSage)
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
 }
