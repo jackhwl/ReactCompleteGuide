02443fa525e999b571b2df50958db80d0e1d8f52
Wenlin Huang <jackhwl@hotmail.com>
2019-12-23 10:00:10 -0500

    jsx is a variable can assign value or loop on it

--- zf-react/src/index.js
+++ zf-react/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let style = { border: '1px solid red' }
-let element1 = <h1
-        style = {style}
-        id="hello"
-        className="title">hello</h1>
-let element2 = React.createElement("h1", null, "heloo")
-element2 = {type: 'h1', props: {children: 'hello'}}
-ReactDOM.render(element1, document.getElementById('root'))
+// let style = { border: '1px solid red' }
+// let element1 = <h1
+//         style = {style}
+//         id="hello"
+//         className="title">hello</h1>
+// let element2 = React.createElement("h1", null, "heloo")
+// element2 = {type: 'h1', props: {children: 'hello'}}
+
+// function greeting(name) {
+//     return (
+//         <div>hello {name}</div>
+//     )
+// }
+// let element1 = greeting('jack')
+
+// let names = ['1', '2', '3']
+// let list = []
+// for (let i = 0; i< names.length; i++) {
+//     list.push(<li key={i}>{names[i]}</li>)
+// }
+// let element1 = <ul>{list}</ul>
+
+
+ReactDOM.render(element1, document.getElementById('root')) 
