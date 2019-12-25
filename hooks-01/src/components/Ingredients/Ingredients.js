4b901ff1f11e3d00ef39e5b42b64a14931d94671
Wenlin Huang <jackhwl@hotmail.com>
2019-12-25 08:50:49 -0500

    463. Sending Http Requests

--- hooks-01/src/components/Ingredients/Ingredients.js
+++ hooks-01/src/components/Ingredients/Ingredients.js
@@ -8,10 +8,18 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState([])
 
   const addIngredientHandler = ingredient => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients, 
-      {id: Math.random().toString(), ...ingredient}
-    ])
+    fetch('https://react-hoos-update.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => response.json())
+    .then(responseData => {
+      setIngredients(prevIngredients => [
+        ...prevIngredients, 
+        {id: responseData.name, ...ingredient}
+      ])
+    })
+    
   }
 
   const removeIngredientHandler = id => {
