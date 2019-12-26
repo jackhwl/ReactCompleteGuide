161071a4000729106613805aea5c61bb45ed9f75
Wenlin Huang <jackhwl@hotmail.com>
2019-12-26 11:32:32 -0500

    471. Loading Errors & State Batching

--- hooks-01/src/components/Ingredients/Ingredients.js
+++ hooks-01/src/components/Ingredients/Ingredients.js
@@ -1,28 +1,15 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react'
 
-import IngredientForm from './IngredientForm';
+import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
+import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
 
-  // useEffect(() => {
-  //   fetch('https://react-hoos-update.firebaseio.com/ingredients.json')
-  //     .then(response => response.json())
-  //     .then(responseData => {
-  //       const loadedIngredients = []
-  //       for(const key in responseData){
-  //         loadedIngredients.push({
-  //           id: key,
-  //           title: responseData[key].title,
-  //           amount: responseData[key].amount
-  //         })
-  //       }
-  //       setIngredients(loadedIngredients)
-  //     })
-  // }, [])
-  
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', ingredients)
   }, [ingredients])
@@ -32,11 +19,15 @@ const Ingredients = () => {
   }, [])
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true)
     fetch('https://react-hoos-update.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {'Content-Type': 'application/json'}
-    }).then(response => response.json())
+    }).then(response => {
+      setIsLoading(false)
+      return response.json()
+    })
     .then(responseData => {
       setIngredients(prevIngredients => [
         ...prevIngredients, 
@@ -47,19 +38,30 @@ const Ingredients = () => {
   }
 
   const removeIngredientHandler = id => {
-    fetch(`https://react-hoos-update.firebaseio.com/ingredients/${id}.json`, {
+    setIsLoading(true)
+    fetch(`https://react-hoos-update.firebaseio.com/ingredients/${id}.son`, {
       method: 'DELETE'
     }).then(response => {
+        setIsLoading(false)
         setIngredients(prevIngredients => 
         prevIngredients.filter(ig => ig.id !== id )
       )
+    }).catch(error => {
+      setError('something went wrong:' + error.message)
+      setIsLoading(false)
     })
+  }
 
+  const clearError = () => {
+    setError(null)
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm 
+        loading={isLoading}
+        onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
