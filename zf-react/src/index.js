b79bee2be16ad706fef99c36e71c3ed7f9c89e43
Wenlin Huang <jackhwl@hotmail.com>
2019-12-27 12:20:18 -0500

    20191222_A_React_组件

--- zf-react/src/index.js
+++ zf-react/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 // 5
 // function Welcome(props) {
@@ -20,3 +21,44 @@ import ReactDOM from 'react-dom'
 // }
 
 // ReactDOM.render(<Welcome name="jack" />, document.getElementById('root'))
+
+//7 pure function
+
+// propeTypes
+
+class Person extends Component {
+    static propTypes = {
+        //age: PropTypes.number.isRequired,
+        gender: PropTypes.oneOf(['male', 'female']).isRequired,
+        hobby: PropTypes.arrayOf(PropTypes.string).isRequired,
+        position: PropTypes.shape({
+            x: PropTypes.number,
+            y: PropTypes.number
+        }),
+        age: function(props, propName, componentName) {
+            if (props.age < 18) {
+                throw new Error('you are a child')
+            }
+        }
+    }
+    render() {
+        return (
+            <div>
+                <span>age: {this.props.age}</span>
+                <span>gender: {this.props.gender}</span>
+                <span>hobby: {this.props.hobby}</span>
+                <span>position: </span>
+            </div>
+        )
+    }
+}
+
+let personProps = {
+    age: 10,
+    gender: 'male',
+    hobby: ['basketball', 'football'],
+    position: {x: 10, y: 10},
+    friends: [{name: 'jone', age: 10}, {name: 'john', age: -20}]
+}
+
+ReactDOM.render(<Person {...personProps} />, document.getElementById('root')) 
