c04beba8afa9c73a2716c160583d984451102c3c
Wenlin Huang <jackhwl@hotmail.com>
2019-12-30 14:50:59 -0500

    render html node

--- zf-react/src/index.js
+++ zf-react/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from './react-dom'
 //let element = <h1 id="title"><span>hello</span><span>world</span></h1>
 let element = React.createElement('h1', {id: 'title'}, 
     React.createElement('span', { style: { color: 'red', backgroundColor: 'yellow'}}, 'hello'), 
-    React.createElement('span', { className: 'world', viData: 'vi-global'}, 'world'))
+    React.createElement('span', { className: 'world', "data-vi-id": 'vi23', id: 'vi-global'}, 'world'))
 console.log(element)
 ReactDOM.render(element, document.getElementById('root'))
