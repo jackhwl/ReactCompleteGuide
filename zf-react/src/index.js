1b1a18228c56e1eccd59285f487295ea8d4d9e2e
Wenlin Huang <jackhwl@hotmail.com>
2019-12-30 12:40:23 -0500

    reactdom.render

--- zf-react/src/index.js
+++ zf-react/src/index.js
@@ -1,9 +1,9 @@
 import React from './react'
-import ReactDOM from 'react-dom'
+import ReactDOM from './react-dom'
 
-let element = <h1 id="title"><span>hello</span><span>world</span></h1>
-element = React.createElement('h1', {id: 'title'}, 
-    React.createElement('span', null, 'hello'), 
-    React.createElement('span', null, 'world'))
+//let element = <h1 id="title"><span>hello</span><span>world</span></h1>
+let element = React.createElement('h1', {id: 'title'}, 
+    React.createElement('span', { style: { color: 'red', backgroundColor: 'yellow'}}, 'hello'), 
+    React.createElement('span', { className: 'world', viData: 'vi-global'}, 'world'))
 console.log(element)
-//ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById('root'))
---
+++ zf-react/src/react-dom/index.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @param {*} node React node, could be number string ...
+ * @param {*} parent  container, a real dom element
+ */
+function render(node, parent) {
+    if (typeof node === 'string') {
+        return parent.appendChild(document.createTextNode(node))
+    }
+    let type, props
+    type = node.type //h1 / Function / ClassComponent
+    props = node.props
+    let domElement = document.createElement(type)
+    for(let propName in props){
+        if (propName === 'children') {
+            let children = props.children
+            if (!Array.isArray(children)) {
+                children = [children]
+            }
+            children.forEach(child => render(child, domElement))
+        } else if (propName === 'className') {
+            domElement.className = props.className 
+        } else if (propName === 'style') {
+            let styleObject = props.style
+            for(let attr in styleObject) {
+                domElement.style[attr] = styleObject[attr]
+            }
+        } else {
+            domElement.setAttribute(propName, props[propName])
+        }
+    }
+    parent.appendChild(domElement)
+}
+
+export default { render }
