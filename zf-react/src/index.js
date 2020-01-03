d986db9c29f1b36dfd75db092141a3cad3bf2797
Wenlin Huang <jackhwl@hotmail.com>
2020-01-03 12:48:05 -0500

    Route Hash Link

--- zf-react/src/index.js
+++ zf-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Route } from './react-router-dom'
+import { HashRouter as Router, Route, Link } from './react-router-dom'
 import Home from './components/Home'
 import User from './components/User'
 import Profile from './components/Profile'
@@ -10,6 +10,9 @@ import Profile from './components/Profile'
  */
 ReactDOM.render(
 <Router>
+    <Link to="/">Home</Link>
+    <Link to="/user">User</Link>
+    <Link to="/profile">Profile</Link>
     <Route path="/" exact component={Home} />
     <Route path="/user" component={User} />
     <Route path="/profile" component={Profile} />
--- zf-react/src/react-router-dom/index.js
+++ zf-react/src/react-router-dom/index.js
@@ -1,7 +1,8 @@
 import HashRouter from './HashRouter'
 import Route from './Route'
-
+import Link from './Link'   
 export {
     HashRouter,
-    Route
+    Route,
+    Link
 }
