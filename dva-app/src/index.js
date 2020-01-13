ea2e0dd5fbf9a2efb8f25159b0b30515d7031a72
Wenlin Huang <jackhwl@hotmail.com>
2020-01-13 08:36:25 -0500

    dva_src

--- dva-app/src/index.js
+++ dva-app/src/index.js
@@ -1,95 +1,37 @@
-import dva, { connect } from 'dva'
 import React from 'react'
+import ReactDOM from 'react-dom'
+import dva, { connect } from 'dva'
 import { Router, Route} from 'dva/router'
-import keymaster from 'keymaster'
-import createLogger from 'redux-logger'
+import {createBrowserHistory} from 'history'
+
 let app = dva({
-    onAction: createLogger
+    history: createBrowserHistory
 })
-const delay = ms => new Promise(function(resolve){
-    setTimeout(()=>{
-        resolve()
-    }, ms)
-})
+
 app.model({
-    namespace: 'counter1',
+    namespace: 'counter',
     state: { number: 0},
     reducers: {
         add(state){
             return { number: state.number + 1}
-        },
-        minus(state){
-            return { number: state.number - 1}
-        },
-        log(state){
-            console.log('reducers log')
-            return { number: 100}
-        }
-    },
-    effects: {
-        *asyncAdd(action, {put, call}) {
-            yield call(delay, 1000)
-            yield put({type: 'add'})
-        },
-        *log(action, {select}){
-            let state = yield select(state=>state.counter1 )
-            console.log('effects log', state)
-        }
-    },
-    subscriptions: {
-        changeTitle({history}){
-             history.listen((location)=> {
-                 console.log(location)
-                 document.title = location.pathname
-             })
-        },
-        keyboard({dispatch}) {
-            keymaster('space', ()=>{
-                dispatch({type: 'add'})
-            })
-        }
-    }
-})
-app.model({
-    namespace: 'counter2',
-    state: { number: 0},
-    reducers: {
-        add(state){
-            return { number: state.number + 1}
-        },
-        minus(state){
-            return { number: state.number - 1}
         }
     }
 })
 
-function Counter1(props){
+function Counter(props){
     return (
         <div>
             <p>{props.number}</p>
-            <button onClick={()=>props.dispatch({type: 'counter1/add'})}>+</button>
-            <button onClick={()=>props.dispatch({type: 'counter1/asyncAdd'})}>async+</button>
-            <button onClick={()=>props.dispatch({type: 'counter1/log'})}>log</button>
-            <button onClick={()=>props.dispatch({type: 'counter1/minus'})}>-</button>
+            <button onClick={()=>props.dispatch({type: 'counter/add'})}>+</button> 
         </div>
     )
 }
-const ConnectedCounter1 = connect(state => state.counter1)(Counter1)
-function Counter2(props){
-    return (
-        <div>
-            <p>{props.number}</p>
-            <button onClick={()=>props.dispatch({type: 'counter2/add'})}>+</button>
-            <button onClick={()=>props.dispatch({type: 'counter2/minus'})}>-</button>
-        </div>
-    )
-}
-const ConnectedCounter2 = connect(state => state.counter2)(Counter2)
-app.router(({history})=> 
-    <Router history={history}>
-        <>
-            <Route path="/counter1" component={ConnectedCounter1} />
-            <Route path="/counter2" component={ConnectedCounter2} />
-        </>
-    </Router>)
+const ConnectedCounter = connect(state => state.counter)(Counter)
+app.router(()=> <ConnectedCounter />)
+// app.router(({history})=> 
+//     <Router history={history}>
+//         <>
+//             <Route path="/counter" component={ConnectedCounter} />
+//         </>
+//     </Router>)
 app.start('#root')
---
+++ dva-app/src/index_dva.js
@@ -0,0 +1,95 @@
+import dva, { connect } from 'dva'
+import React from 'react'
+import { Router, Route} from 'dva/router'
+import keymaster from 'keymaster'
+import createLogger from 'redux-logger'
+let app = dva({
+    onAction: createLogger
+})
+const delay = ms => new Promise(function(resolve){
+    setTimeout(()=>{
+        resolve()
+    }, ms)
+})
+app.model({
+    namespace: 'counter1',
+    state: { number: 0},
+    reducers: {
+        add(state){
+            return { number: state.number + 1}
+        },
+        minus(state){
+            return { number: state.number - 1}
+        },
+        log(state){
+            console.log('reducers log')
+            return { number: 100}
+        }
+    },
+    effects: {
+        *asyncAdd(action, {put, call}) {
+            yield call(delay, 1000)
+            yield put({type: 'add'})
+        },
+        *log(action, {select}){
+            let state = yield select(state=>state.counter1 )
+            console.log('effects log', state)
+        }
+    },
+    subscriptions: {
+        changeTitle({history}){
+             history.listen((location)=> {
+                 console.log(location)
+                 document.title = location.pathname
+             })
+        },
+        keyboard({dispatch}) {
+            keymaster('space', ()=>{
+                dispatch({type: 'add'})
+            })
+        }
+    }
+})
+app.model({
+    namespace: 'counter2',
+    state: { number: 0},
+    reducers: {
+        add(state){
+            return { number: state.number + 1}
+        },
+        minus(state){
+            return { number: state.number - 1}
+        }
+    }
+})
+
+function Counter1(props){
+    return (
+        <div>
+            <p>{props.number}</p>
+            <button onClick={()=>props.dispatch({type: 'counter1/add'})}>+</button>
+            <button onClick={()=>props.dispatch({type: 'counter1/asyncAdd'})}>async+</button>
+            <button onClick={()=>props.dispatch({type: 'counter1/log'})}>log</button>
+            <button onClick={()=>props.dispatch({type: 'counter1/minus'})}>-</button>
+        </div>
+    )
+}
+const ConnectedCounter1 = connect(state => state.counter1)(Counter1)
+function Counter2(props){
+    return (
+        <div>
+            <p>{props.number}</p>
+            <button onClick={()=>props.dispatch({type: 'counter2/add'})}>+</button>
+            <button onClick={()=>props.dispatch({type: 'counter2/minus'})}>-</button>
+        </div>
+    )
+}
+const ConnectedCounter2 = connect(state => state.counter2)(Counter2)
+app.router(({history})=> 
+    <Router history={history}>
+        <>
+            <Route path="/counter1" component={ConnectedCounter1} />
+            <Route path="/counter2" component={ConnectedCounter2} />
+        </>
+    </Router>)
+app.start('#root')
