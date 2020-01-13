669ad8916df3b8a0ab785bcc268fa452343a3abc
Wenlin Huang <jackhwl@hotmail.com>
2020-01-13 06:49:18 -0500

    dva route

--- dva-app/src/index.js
+++ dva-app/src/index.js
@@ -1,5 +1,6 @@
 import dva, { connect } from 'dva'
 import React from 'react'
+import { Router, Route} from 'dva/router'
 
 let app = dva()
 app.model({
@@ -27,5 +28,31 @@ app.model({
     }
 })
 
-app.router(()=> <div>component</div>)
+function Counter1(props){
+    return (
+        <div>
+            <p>{props.number}</p>
+            <button onClick={()=>props.dispatch({type: 'counter1/add'})}>+</button>
+            <button onClick={()=>props.dispatch({type: 'counter1/minus'})}>-</button>
+        </div>
+    )
+}
+const ConnectedCounter1 = connect(state => state.counter1)(Counter1)
+function Counter2(props){
+    return (
+        <div>
+            <p>{props.number}</p>
+            <button onClick={()=>props.dispatch({type: 'counter2/add'})}>+</button>
+            <button onClick={()=>props.dispatch({type: 'counter2/minus'})}>-</button>
+        </div>
+    )
+}
+const ConnectedCounter2 = connect(state => state.counter2)(Counter2)
+app.router(({history})=> 
+    <Router history={history}>
+        <>
+            <Route path="/counter1" component={ConnectedCounter1} />
+            <Route path="/counter2" component={ConnectedCounter2} />
+        </>
+    </Router>)
 app.start('#root')
