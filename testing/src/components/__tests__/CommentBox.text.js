1a4730f7acbb4720bd01ea9dfce391bebb95fa1a
Wenlin Huang <jackhwl@hotmail.com>
2020-01-19 08:09:33 -0500

    37. Retrieving Prop Values

--- testing/src/components/__tests__/CommentBox.text.js
+++ testing/src/components/__tests__/CommentBox.text.js
@@ -13,8 +13,14 @@ afterEach(() => {
 })
 
 it('should has a text area and a button ', () => {
-
-    expect(wrapped.find("textarea").length).toEqual(1)
-    expect(wrapped.find("button").length).toEqual(1)
-
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
 })
+
+it('should has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: {value: 'new comment'}
+    })
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+});
