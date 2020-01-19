4abf7bd1273b8fd11bdd267d9599acf00aba5c67
Wenlin Huang <jackhwl@hotmail.com>
2020-01-19 09:18:54 -0500

    43. The SaveComment Action Creator

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-reduc'
+import { createStore } from 'redux'
+import reducers from 'reducers'
 
 import App from 'components/App';
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers, {})}>
+        <App />
+    </Provider>
+, document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
