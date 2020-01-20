a72814db3d89ec7344db43a9852d841122aba34d
Wenlin Huang <jackhwl@hotmail.com>
2020-01-20 07:30:11 -0500

    49. Testing Action Creators

--- testing/src/reducers/__tests__/comments.test.js
+++ testing/src/reducers/__tests__/comments.test.js
@@ -11,3 +11,9 @@ it('should handles actions of type SAVE_COMMENT', () => {
 
     expect(newState).toEqual(['New Comment'])
 })
+
+it('should handles action with unknown type', () => {
+    const newState = commentsReducer([], {})
+
+    expect(newState).toEqual([])
+})
