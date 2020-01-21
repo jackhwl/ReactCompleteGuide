8fb9d957a267c3c6a31163e40ef486122ad01e0d
Wenlin Huang <jackhwl@hotmail.com>
2020-01-21 07:49:35 -0500

    64. Introducing a Pause

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import moxios from 'moxios'
 import Root from 'Root'
 import App from 'components/App'
 
-it('should fetch a list of comments and display them', () => {
+beforeEach(() => {
+    moxios.install()
+    moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{name: 'Fetched #1'},{name: 'Fetched #2'}]
+    })
+})
+
+afterEach(() => {
+    moxios.uninstall()
+})
+
+it('should fetch a list of comments and display them', (done) => {
     const wrapped = mount(
         <Root>
             <App />
         </Root>
     )
+
+    wrapped.find('.fetch-comments').simulate('click')
+    setTimeout(()=> {
+        wrapped.update()
+        expect(wrapped.find('li').length).toEqual(2)
+        done()
+    }, 100)
+    
 })
