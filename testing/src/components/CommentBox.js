28fc8cedca8fdf15b4d5f1018a814e28ab643b25
Wenlin Huang <jackhwl@hotmail.com>
2020-01-23 17:45:31 -0500

    13. Placing Reusable Logic

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -5,17 +5,6 @@ import * as actions from 'actions'
 class CommentBox extends Component {
     state = { comment: ''}
 
-    componentDidMount() {
-        this.shouldNavigateAway()
-    }
-    componentDidUpdate() {
-        this.shouldNavigateAway()
-    }
-    shouldNavigateAway() {
-        if (!this.props.auth) {
-            this.props.history.push('/')
-        }
-    }
     handleChange = event => {
         this.setState({ comment: event.target.value })
     }
@@ -40,8 +29,4 @@ class CommentBox extends Component {
     }
 }
 
-function mapStateToProps(state){
-    return { auth: state.auth }
-}
-
-export default connect(mapStateToProps, actions)(CommentBox)
+export default connect(null, actions)(CommentBox)
