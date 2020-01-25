b684f9a4f4ce3ecd16fa8fa9f3e6e70776f070bc
Wenlin Huang <jackhwl@hotmail.com>
2020-01-25 10:04:25 -0500

    creating-a-search-component

---
+++ complete-intro-to-react-v5-FM/src/SearchParams.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const SearchParams = () => {
+  const location = "Seattle, WA"
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input id="location" value={location} placeholder="Location" />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default SearchParams
