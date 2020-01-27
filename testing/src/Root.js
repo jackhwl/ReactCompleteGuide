42736f6235ad05b5342463cf8ff0ba4cf56594cb
Wenlin Huang <jackhwl@hotmail.com>
2020-01-27 08:34:03 -0500

    12. Emitting Warnings

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise from 'redux-promise'
+import async from 'middlewares/async'
+import stateValidator from 'middlewares/stateValidator'
+//import reduxPromise from 'redux-promise'
 import reducers from 'reducers'
 
 export default ({children, initialState = {}}) => {
-    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise))
+    const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator))
     return (
         <Provider store={store} >
             {children}
