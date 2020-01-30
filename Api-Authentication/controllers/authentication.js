e558784a701be7b55cdbb17d5da75d24d87e9958
Wenlin Huang <jackhwl@hotmail.com>
2020-01-30 06:42:32 -0500

    14. Encrypting Passwords with Bcrypt

--- Api-Authentication/controllers/authentication.js
+++ Api-Authentication/controllers/authentication.js
@@ -2,6 +2,10 @@ const User = require('../models/user')
 
 exports.signup = function(req, res, next) {
     const {email, password } = req.body
+
+    if(!email || !password){
+        return res.status(422).send({error: 'You must provide email and password'})
+    }
     // See if a user with the given email exists
     User.findOne({email}, function(err, existingUser) {
         if (err) { return next(err)}
