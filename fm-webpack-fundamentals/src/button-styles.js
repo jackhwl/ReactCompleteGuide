87f073d6311ec6f949cd7a20fa379ebaf2c72055
Wenlin Huang <jackhwl@hotmail.com>
2020-01-31 08:33:52 -0500

    Tree Shaking

--- fm-webpack-fundamentals/src/button-styles.js
+++ fm-webpack-fundamentals/src/button-styles.js
@@ -2,6 +2,7 @@ const red = "color: red;"
 const blue = "color: blue"
 const makeColorStyle = (color) => `color: ${color};`
 
-exports.red = red
-exports.blue = blue
-exports.makeColorStyle = makeColorStyle
+// exports.red = red
+// exports.blue = blue
+// exports.makeColorStyle = makeColorStyle
+export {red, blue, makeColorStyle}
