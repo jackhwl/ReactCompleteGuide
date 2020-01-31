8b0adb9cbddaf4f869d072d116d8fbe5156693e3
Wenlin Huang <jackhwl@hotmail.com>
2020-01-30 21:21:45 -0500

    commonjs-named-exports

---
+++ fm-webpack-fundamentals/src/button.js
@@ -0,0 +1,11 @@
+// take a str, then button label and return a element
+/**
+ * 
+ * @param {string} buttonName 
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+    return `Button: ${buttonName}`
+}
+
+module.exports = makeButton
