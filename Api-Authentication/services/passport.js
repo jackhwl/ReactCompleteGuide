587cbf3301f7e88fb6b905fcd0443138ecc70d4d
Wenlin Huang <jackhwl@hotmail.com>
2020-02-01 11:59:09 -0500

    114. Signing in with Local Strategy

--- Api-Authentication/services/passport.js
+++ Api-Authentication/services/passport.js
@@ -3,7 +3,16 @@ const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const LocalStrategy = require('passport-local')
 
+// Create local strategy
+const localOption = {usernameField: 'email'}
+const LocalStrategy = new LocalStrategy(localOption, function(email, password, done){
+    // Verify this username and password, call done with the user
+    // if it is the correct username and password
+    // otherwise, call done with false
+    
+})
 // Setup options for JWT Strategy
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
