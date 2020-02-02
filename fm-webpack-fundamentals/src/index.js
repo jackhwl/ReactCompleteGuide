63a59f67ff65aeef9414579172274ed147729c4e
Wenlin Huang <jackhwl@hotmail.com>
2020-02-02 14:52:40 -0500

    setting-up-a-local-development-server

--- fm-webpack-fundamentals/src/index.js
+++ fm-webpack-fundamentals/src/index.js
@@ -3,6 +3,8 @@ import { top, bottom } from './footer'
 import makeButton from './button'
 import { makeColorStyle} from './button-styles'
 
+const button = makeButton("Yay! A Button!")
+document.body.appendChild(button)
 console.log(nav(), top, bottom, 
     makeButton("My first button!"),
     makeColorStyle("cyan"))
