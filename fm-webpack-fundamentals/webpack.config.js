d3aae05a9a609692199c60bcde7f078f1774d7b6
Wenlin Huang <jackhwl@hotmail.com>
2020-02-02 13:48:45 -0500

    passing-variable-to-webpack-config

--- fm-webpack-fundamentals/webpack.config.js
+++ fm-webpack-fundamentals/webpack.config.js
@@ -1,5 +1,9 @@
-module.exports = () => ({
-    output: {
-        filename: "bundle.js"
+module.exports = ({mode}) => {
+    console.log(mode)
+    return {
+        mode,
+        output: {
+            filename: "bundle.js"
+        }
     }
-})
+}
