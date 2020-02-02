3053c57f3825e522d183a3bff5ae46e29557201d
Wenlin Huang <jackhwl@hotmail.com>
2020-02-02 15:01:53 -0500

    starting-to-code-with-webpack

--- fm-webpack-fundamentals/src/footer.js
+++ fm-webpack-fundamentals/src/footer.js
@@ -1,4 +1,14 @@
-const top = "top"
-const bottom = "bottom"
+import { red, blue } from './button-styles'
 
-export {top, bottom}
+const top = document.createElement("div")
+top.innerText = "Top of Footer"
+top.style = red
+const bottom = document.createElement("div")
+bottom.innerHTML = "Bottom of Footer"
+bottom.style = blue
+
+const footer = document.createElement("div")
+footer.appendChild(top)
+footer.appendChild(bottom)
+
+export {top, bottom, footer}
--- fm-webpack-fundamentals/src/index.js
+++ fm-webpack-fundamentals/src/index.js
@@ -1,10 +1,12 @@
 import nav from './nav'
-import { top, bottom } from './footer'
+import { footer } from './footer'
 import makeButton from './button'
 import { makeColorStyle} from './button-styles'
 
 const button = makeButton("Yay! A Button!")
+button.style = makeColorStyle("red")
 document.body.appendChild(button)
-console.log(nav(), top, bottom, 
-    makeButton("My first button!"),
-    makeColorStyle("cyan"))
+document.body.appendChild(footer)
+// console.log(nav(), top, bottom, 
+//     makeButton("My first button!"),
+//     makeColorStyle("cyan"))
