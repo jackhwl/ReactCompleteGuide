60a1400d34ff782a7d5766ea47114496a27fa98c
Wenlin Huang <jackhwl@hotmail.com>
2020-02-04 20:11:50 -0500

    hot-module-replacement-with-css

--- fm-webpack-fundamentals/build-utils/webpack.production.js
+++ fm-webpack-fundamentals/build-utils/webpack.production.js
@@ -1,5 +1,15 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 module.exports = () => ({
     output: {
         filename: "[chunkhash].js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [ new MiniCssExtractPlugin()]
 })
--- fm-webpack-fundamentals/src/index.js
+++ fm-webpack-fundamentals/src/index.js
@@ -2,6 +2,8 @@ import nav from './nav'
 import { footer } from './footer'
 import makeButton from './button'
 import { makeColorStyle} from './button-styles'
+import buttonStyles from './button.css'
+import './footer.css'
 
 const button = makeButton("Yay! A Button!")
 button.style = makeColorStyle("red")
