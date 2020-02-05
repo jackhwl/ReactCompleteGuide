eaf853d12c13d0b778b04a6ca0ae811cce00e535
Wenlin Huang <jackhwl@hotmail.com>
2020-02-05 06:55:05 -0500

    9. Handling Form Submission

--- client-authentication/src/components/auth/Signup.js
+++ client-authentication/src/components/auth/Signup.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
 class Signup extends Component {
+    onSubmit = formProps => {
+        console.log(formProps)
+    }
     render() {
+        const { handleSubmit } = this.props
         return (
-            <form>
+            <form onSubmit = {handleSubmit(this.onSubmit)}>
                 <fieldset>
                     <label>Email</label>
                     <Field name="email" type="text" component="input" autoComplete="none" />
@@ -14,6 +18,7 @@ class Signup extends Component {
                     <label>Password</label>
                     <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
+                <button>Sign Up!</button>
             </form>
         )
     }
