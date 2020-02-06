b1f04c7c518ae90cd589b862fda79806ac33dee7
Wenlin Huang <whuang@viglobal.com>
2020-02-06 06:53:34 -0500

    13. Code Cleanup with Compose

--- client-authentication/src/index.js
+++ client-authentication/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware  } from 'redux'
-import { reduxThunk } from "redux-thunk";
+import reduxThunk from "redux-thunk";
 
 import reducers from './reducers'
 import App from './components/App'
