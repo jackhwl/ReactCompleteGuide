6a33f04ff753a06c88d3383c04b9b465f85860db
Wenlin Huang <whuang@viglobal.com>
2020-02-07 09:12:00 -0500

    implementing-presets

--- fm-webpack-fundamentals/webpack.config.js
+++ fm-webpack-fundamentals/webpack.config.js
@@ -1,7 +1,9 @@
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpackMerge = require("webpack-merge")
+
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env)
+const presetConfig = require("./build-utils/loadPresets")
 
 module.exports = ({mode, presets} = { mode: "production", presets: []}) => {
     return webpackMerge({
@@ -12,10 +14,10 @@ module.exports = ({mode, presets} = { mode: "production", presets: []}) => {
                         test: /\.jpe?g$/,
                         use: [
                             {
-                            loader: "url-loader",
-                            options: {
-                                limit: 5000
-                            }
+                                loader: "url-loader",
+                                options: {
+                                    limit: 5000
+                                }
                             }
                         ]
                     }
@@ -29,6 +31,7 @@ module.exports = ({mode, presets} = { mode: "production", presets: []}) => {
                 new webpack.ProgressPlugin()
             ]
         },
-        modeConfig(mode)
+        modeConfig(mode),
+        presetConfig({mode, presets})
     )
 }
