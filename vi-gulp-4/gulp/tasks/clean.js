e95d5e580b3ac4f74066a5dcbab512891587d748
Wenlin Huang <whuang@viglobal.com>
2020-03-02 09:35:22 -0500

    refine clean task

--- vi-gulp-4/gulp/tasks/clean.js
+++ vi-gulp-4/gulp/tasks/clean.js
@@ -4,34 +4,24 @@
 // npm install --save-del del gulp-load-plugins
 // =========================================================
 
-var gulp = require('gulp');
-//var conf = require('./conf');
+var gulp = require('gulp')
+var path = require('path')
+var conf = require('../conf')
 
-// var $ = require('gulp-load-plugins')({
-//   pattern: ['gulp-*', 'del']
-// });
-
-//var //config              = require('../config.js'),
-//   del                 = require('del');
 var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del', 'minimist', 'lazypipe']
-  });
+  pattern: ['gulp-*', 'del']
+})
 
 module.exports = function() {
     return function (cb) {
-    var stream = 
-// -------------------------------------------- Start Task
-        // del(config.clean.folders, cb);
-        $.del('./dist/', cb);
-// ---------------------------------------------- End Task
-    return stream;
-    };
-};
-
-//gulp.task('clean',      clean());
-
-// gulp.task('clean', function () {
-//     return $.del([path.join(conf.paths.dist, '/')
-//       , path.join(conf.paths.tmp, '/')
-//     ]);
-//   });
+        var stream = 
+        // -------------------------------------------- Start Task
+            // del(config.clean.folders, cb);
+            $.del([path.join(conf.paths.dist, '/')
+                    , path.join(conf.paths.tmp, '/')
+                ], cb)
+            //$.del('./dist/', cb);
+        // ---------------------------------------------- End Task
+        return stream
+    }
+}
--- vi-gulp-4/gulpfile.js
+++ vi-gulp-4/gulpfile.js
@@ -9,12 +9,14 @@ var gulp = require('gulp'),
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-fs.readdirSync('./gulp/tasks').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  gulp.task(path.parse(file).name, require('./gulp/tasks/' + file)());
-});
+fs.readdirSync('./gulp/tasks')
+    .filter(function(file) {
+        return (/\.(js|coffee)$/i).test(file);
+    })
+    .map(function(file) {
+        gulp.task(path.parse(file).name, require('./gulp/tasks/' + file)())
+    })
 
 gulp.task('default', gulp.series(
 	 'clean')
-);
+)
