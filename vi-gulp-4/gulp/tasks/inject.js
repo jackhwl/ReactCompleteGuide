3715532ef12f416256ef4fadcc942f3ca3c32fbf
Wenlin Huang <whuang@viglobal.com>
2020-03-04 08:18:57 -0500

    add inject task

---
+++ vi-gulp-4/gulp/tasks/inject.js
@@ -0,0 +1,90 @@
+var path = require("path")
+var conf = require("../conf")
+var config = require("../config")
+var gulp = require("gulp")
+var wiredep = require("wiredep").stream
+
+var $ = require("gulp-load-plugins")({
+  pattern: ["gulp-*", "minimist"]
+})
+
+var argv = $.minimist(process.argv.slice(2))
+var debug = argv._.indexOf("debug") == -1 ? false : true
+
+var injectStyles = gulp.src(
+  [
+    path.join(conf.paths.src, "/app/**/*.css")
+    //path.join(conf.paths.tmp, '/serve/**/*.css')
+    //path.join(conf.paths.tmp, '/serve/**/*.scss')
+  ],
+  {
+    read: false
+  }
+)
+
+var injectStylesOptions = {
+  ignorePath: [conf.paths.src, path.join(conf.paths.tmp, "/serve")],
+  addRootSlash: false
+}
+
+var injectEnviormentScript = gulp.src([
+  path.join(
+    conf.paths.src,
+    "/app/**/",
+    debug ? conf.files.devenv : conf.files.env
+  )
+])
+
+var injectEnvOptions = {
+  ignorePath: [conf.paths.src, path.join(conf.paths.tmp, "/serve")],
+  addRootSlash: false,
+  starttag: debug ? "<!-- inject:devenv:js -->" : "<!-- inject:env:js -->"
+}
+
+var injectScripts = gulp
+  .src([
+    path.join(conf.paths.src, "/app/**/*.module.js"),
+    path.join(conf.paths.src, "/app/**/*.js"),
+    path.join("!" + conf.paths.src, "/app/**/env.js"),
+    path.join("!" + conf.paths.src, "/app/**/devenv.js"),
+    path.join("!" + conf.paths.src, "/app/**/*.spec.js"),
+    path.join("!" + conf.paths.src, "/app/**/*.mock.js")
+  ])
+  .pipe($.sort())
+  .on("error", conf.errorHandler("AngularFilesort"))
+
+var injectOptions = {
+  ignorePath: [conf.paths.src, path.join(conf.paths.tmp, "/serve")],
+  addRootSlash: false
+}
+
+module.exports = function() {
+  return function() {
+    // prettier-ignore
+    var stream =
+      // -------------------------------------------- Start Task
+      gulp
+        .src([
+          path.join(conf.paths.src, "/*.html"),
+          path.join(conf.paths.src, "/*.scss")
+        ])
+        .pipe($.if(!debug, $.stripCode(conf.stripCodePattern("start spa css", "end spa css"))))
+        .pipe($.if(debug, $.stripCode(conf.stripCodePattern("start mvc css", "end mvc css"))))
+        .pipe($.if(debug, $.stripCode(conf.stripCodePattern("start base href", "end base href"))))
+        .pipe($.if(debug, $.stripCode(conf.stripCodePattern("start menu bar", "end menu bar"))))
+        .pipe($.if(debug,$.stripCode(conf.stripCodePattern("start release env", "end release env"))))
+        .pipe($.if(!debug,$.stripCode(conf.stripCodePattern("start dev env", "end dev env"))))
+        .pipe($.inject(injectStyles, injectStylesOptions))
+        .pipe($.inject(injectEnviormentScript, injectEnvOptions))
+        .pipe($.inject(injectScripts, injectOptions))
+
+        .pipe(wiredep(Object.assign({}, config.wiredepOptions(!debug))))
+        .on("error", conf.errorHandler("wiredep"))
+        // .pipe($.replace(/<ov-app-loading>/, function(s){
+        //     return fs.readFileSync(path.join(conf.paths.src, '/app/components/loading/loading.htm'), 'utf8');
+        // }))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, "/serve")))
+    // ---------------------------------------------- End Task
+    return stream
+  }
+}
--- vi-gulp-4/gulpfile.js
+++ vi-gulp-4/gulpfile.js
@@ -1,29 +1,30 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+var fs = require("fs")
+var path = require("path")
+var gulp = require("gulp"),
+  plugins = require("gulp-load-plugins")()
 
-var fs = require('fs');
-var path = require('path');
-var gulp = require('gulp'), 
-    plugins		= require('gulp-load-plugins')();
-
-var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'minimist']
-    })
+var $ = require("gulp-load-plugins")({
+  pattern: ["gulp-*", "minimist"]
+})
 //http://gunargessner.com/gulp-release
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
 
- // Parse the arguments to check for 'release'
-var argv = $.minimist(process.argv.slice(2));
-var release = (argv._.indexOf('release') == -1 ? false : true);
+// Parse the arguments to check for 'release'
+var argv = $.minimist(process.argv.slice(2))
+var release = argv._.indexOf("release") == -1 ? false : true
 
-fs.readdirSync('./gulp/tasks')
-    .filter(function(file) {
-        return (/\.(js|coffee)$/i).test(file);
-    })
-    .map(function(file) {
-        gulp.task(path.parse(file).name, require('./gulp/tasks/' + file)())
-    })
+fs.readdirSync("./gulp/tasks")
+  .filter(function(file) {
+    return /\.(js|coffee)$/i.test(file)
+  })
+  .map(function(file) {
+    gulp.task(path.parse(file).name, require("./gulp/tasks/" + file)())
+  })
 
 // gulp.task('mode', async function () {
 //     return function(cb) {
@@ -35,15 +36,18 @@ fs.readdirSync('./gulp/tasks')
 //     }
 // });
 if (release) {
-    $.util.log( $.util.colors.red('RUNNING IN RELEASE MODE')) 
+  $.util.log($.util.colors.red("RUNNING IN RELEASE MODE"))
 } else {
-    $.util.log( $.util.colors.green('RUNNING IN DEBUG MODE'))
+  $.util.log($.util.colors.green("RUNNING IN DEBUG MODE"))
 }
 
-gulp.task('build', gulp.series('clean', 'sass', 'scripts'));
+gulp.task(
+  "build",
+  gulp.series("clean", gulp.parallel("sass", "scripts"), "inject")
+)
 
-gulp.task('release', gulp.series('build'));
-  
-gulp.task('debug', gulp.series('build'));
+gulp.task("release", gulp.series("build"))
 
-gulp.task('default', gulp.series('debug'));
+gulp.task("debug", gulp.series("build"))
+
+gulp.task("default", gulp.series("debug"))
