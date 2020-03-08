7cd4743e595642e481a733fcfb1646a0a55f9544
Wenlin Huang <jackhwl@hotmail.com>
2020-03-08 19:35:50 -0400

    videsktop.css and map

--- vi-gulp-4/gulp/tasks/sass.js
+++ vi-gulp-4/gulp/tasks/sass.js
@@ -8,6 +8,8 @@ var $ = require("gulp-load-plugins")({
   pattern: ["gulp-*", "minimist"]
 })
 
+$.sass.compiler = require("dart-sass")
+
 var argv = $.minimist(process.argv.slice(2))
 var release = argv._.indexOf("release") == -1 ? false : true
 
@@ -16,10 +18,28 @@ module.exports = function() {
     var stream =
       // -------------------------------------------- Start Task
       gulp
-        .src(path.join(conf.paths.src, "/*.scss"))
+        .src([
+          path.join(conf.paths.src, "*.scss"),
+          path.join(conf.paths.src, "sass/videsktop.scss")
+        ])
         .pipe(wiredep(Object.assign({}, config.wiredepOptions(release))))
         .on("error", conf.errorHandler("wiredep"))
+        .pipe($.if(conf.userev, $.rev()))
+        //.pipe(gulp.dest(path.join(conf.paths.dist, "/")))
+        .pipe($.if(release, $.sourcemaps.init({ loadMaps: true })))
         .pipe($.sass())
+        .pipe($.if(release, $.sourcemaps.write(".")))
+        // comment below to generate file
+        .pipe(
+          $.if(
+            conf.userev,
+            $.rev.manifest("rev-manifest.json", {
+              //base: ".",
+              merge: true
+            })
+          )
+        )
+        // .pipe(gulp.dest(path.join(conf.paths.dist, "/")))
         .pipe(gulp.dest(path.join(conf.paths.tmp, "/serve")))
     //.pipe(gulp.dest(conf.paths.tmp, "/serve"))
     // ---------------------------------------------- End Task
--- vi-gulp-4/gulpfile.js
+++ vi-gulp-4/gulpfile.js
@@ -19,7 +19,7 @@ fs.readdirSync("./gulp/tasks")
 
 gulp.task(
   "build",
-  series("clean", parallel("sass"), "inject", "manifest", "html") //, "partials", "html"
+  series("clean", "sass", "inject", "manifest", "html") //, "partials", "html"
 )
 
 gulp.task("release", parallel("mode", "build"))
